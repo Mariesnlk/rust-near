a8c0ddb77657ef96ddad512aba0de0193443aee1
mariia.synelnyk <44849051+Mariesnlk@users.noreply.github.com>
2023-05-05 10:36:41 +0200

    add skeleton

---
+++ minting/nft-contract/src/mint.rs
@@ -0,0 +1,32 @@
+use crate::*;
+
+#[near_bindgen]
+impl Contract {
+    #[payable]
+    pub fn nft_mint(
+        &mut self,
+        token_id: Option<TokenId>,
+        metadata: TokenMetadata,
+        receiver_id: Option<AccountId>,
+    ) {
+       let initial_storage_usage = env::storage_usage();
+
+       let token = Token {
+        owner_id: receiver_id,
+       };
+
+       // like require - that token doesn't exist
+       assert!(
+        self.tokens_by_id.insert(&token_id, &metadata);
+
+        //call the internal method for adding the token to the owner
+        self.internal_add_token_to_owner(&token.owner_id, &token_id);
+
+        //calculate the required storage which was the used - initial
+        let required_storage_in_bytes = env::storage_usage() - initial_storage_usage;
+
+        //refund any excess storage if the user attached too much. Panic if they didn't attach enough to cover the required.
+        refund_deposit(required_storage_in_bytes);
+       )
+    }
+}
